2c3e80e852b4f596ffe98cda8cad64574611cac6
Bagus Almahenzar <221911108@stis.ac.id>
2022-03-17 10:32:18 +0700

    Video 11: N+1 Problem

--- routes/web.php
+++ routes/web.php
@@ -39,10 +39,12 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 //halaman single category
 Route::get('categories/{category:slug}', function (Category $category) {
-    return view('category', [
-        'title' => $category->name,
-        'posts' => $category->posts,
-        'category' => $category->name
+    return view('posts', [
+        'title' => 'Post by Category :' . $category->name,
+        'posts' => $category->posts->load([
+            'author',
+            'category'
+        ])
     ]);
 });
 
@@ -56,8 +58,11 @@ Route::get('/categories', function () {
 
 // halaman single user
 Route::get('/authors/{author:username}', function (User $author) {
-    return view('users', [
-        'title' => 'Post Users',
-        'posts' => $author->posts
+    return view('posts', [
+        'title' => 'Post by Author : ' . $author->name,
+        'posts' => $author->posts->load([
+            'category',
+            'author'
+        ]),
     ]);
 });
