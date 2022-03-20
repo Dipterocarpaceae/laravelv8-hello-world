f9cc40c621fb0a1ffaa4661ac342e2b904190e58
Dipterocarpaceae <bagusalmahenzar@gmail.com>
2022-03-20 09:24:21 +0700

    Video 16: User Login & Middleware

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -12,5 +13,30 @@ class LoginController extends Controller
             'title' => 'Login',
             'active' => 'login',
         ]);
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email:dns',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate(); //Menghindari teknik hacking session fixation
+            return redirect()->intended('/dashboard');
+        }
+
+        return redirect()->back()->with([
+            'loginError' => 'Login failed!',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate(); //matiin session
+        $request->session()->regenerateToken(); //generate token baru
+        return redirect('/');
     }
 }
